47db92cc1047005dc048ccc8ea3014d2210b6fd8
Aleksander Arsentev <sazeks.gremory@gmail.com>
2022-07-16 16:12:09 +0300

    Задания от 1 до 3

---
+++ Task_2/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число: "); // Ввод значения пользователем
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(number > 99 ?
+    $"{number} -> {number.ToString()[2]}" :
+    $"{number} -> третьей цифры нет");
